7b99e4a01491e0cdc5f14f59f7c3abc20d629b53
Thomas.dryjard <newwbuiss@gmail.com>
2020-05-22 16:05:03 +0200

    change cors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const api = require('./routes');
 
 const port = process.env.PORT || 8000;
 
-app.use(cors());
+app.use(cors({ origin: 'https://sortouch-front.herokuapp.com/' }));
 
 app.use(bodyParser.json());
 
