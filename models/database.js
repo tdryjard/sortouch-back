ab7142f4bf84246ae4bd6b2b8a179eaa8360aa4b
Thomas.dryjard <newwbuiss@gmail.com>
2020-05-27 14:17:28 +0200

    add handleDisconect

--- models/database.js
+++ models/database.js
@@ -1,18 +1,37 @@
 const mysql = require('mysql');
 
-const connection = mysql.createPool({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
 
-connection.query('select 1 + 1', (err, rows) => {
+connection.connect(err => {
   if (err) {
-    console.log(err)
     throw err;
   }
   console.log('Successfully connected to the database');
 });
+
+function handleDisconnect(conn) {
+  conn.on('error', function(err) {
+    if (!err.fatal) {
+      return;
+    }
+
+    if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
+      throw err;
+    }
+
+    console.log('Re-connecting lost connection: ' + err.stack);
+
+    connection = mysql.createConnection(conn.config);
+    handleDisconnect(connection);
+    connection.connect();
+  });
+}
+
+handleDisconnect(connection);
 
 module.exports = connection;
