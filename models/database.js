582d53cb0830054e482637c67c0f24a465324fba
Thomas.dryjard <newwbuiss@gmail.com>
2020-05-27 19:42:43 +0200

    test

--- models/database.js
+++ models/database.js
@@ -1,37 +1,16 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({ 
+  connectionLimit: 5,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
+})
+
+connection.getConnection(function(err, connection) {
+  if (err) throw err;
+  else console.log('connected')
 });
-
-connection.connect(err => {
-  if (err) {
-    throw err;
-  }
-  console.log('Successfully connected to the database');
-});
-
-function handleDisconnect(conn) {
-  conn.on('error', function(err) {
-    if (!err.fatal) {
-      return;
-    }
-
-    if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
-      throw err;
-    }
-
-    console.log('Re-connecting lost connection: ' + err.stack);
-
-    connection = mysql.createConnection(conn.config);
-    handleDisconnect(connection);
-    connection.connect();
-  });
-}
-
-handleDisconnect(connection);
 
 module.exports = connection;
