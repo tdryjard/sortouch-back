cdc1da70163ab328a6dfa4c6835fe83654b560e6
Thomas.dryjard <newwbuiss@gmail.com>
2020-05-29 01:28:59 +0200

    change cors

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const api = require('./routes');
 
 const port = process.env.PORT || 8080
 
-app.use(cors({origin: 'https://sortouch.co'}))
+app.use(cors({origin: 'https://api.sortouch.co'}))
 
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('https://api.sortouch.co/api', api);
+app.use('/api', api);
 
 
 app.listen(port, () => console.log(`Server is running on port ${port}.`));
