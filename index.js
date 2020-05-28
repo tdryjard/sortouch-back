d3913480f97383f660870d8866c485bb283df677
Thomas.dryjard <newwbuiss@gmail.com>
2020-05-28 15:42:53 +0200

    add cors

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const api = require('./routes');
 
 const port = process.env.PORT || 8080
 
+app.use(cors({origin: 'https://sortouch.co'}))
+
 app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api', api);
 
