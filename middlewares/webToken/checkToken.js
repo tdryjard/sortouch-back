5d43d818ed9209f38647dc2fe7ce3b40f6ed2e7f
Thomas.dryjard <newwbuiss@gmail.com>
2020-05-30 00:38:29 +0200

    ajout us53

--- middlewares/webToken/checkToken.js
+++ middlewares/webToken/checkToken.js
@@ -1,22 +1,15 @@
 let jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-module.exports = function checkToken (req, res) {
-  let token = req.headers['authorization']; // Express headers are auto converted to lowercase
+module.exports = function checkToken(req, res, next) {
+  // Gather the jwt access token from the request header
+  const token = req.headers['authorization']
+  console.log(token)
+  if (token == null) return false
 
-  let success = false
-
-  if (token) {
-    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
-      if (err) {
-        success = false
-      } else {
-        success = true
-        console.log(decoded)
-      }
-    });
-  } else {
-    success = false
-  }
-  return success
-};
+  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+    console.log(err)
+    if (err) return false
+    else return true
+  })
+}
