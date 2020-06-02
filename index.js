eb1cf4f66b64b95b882737346d194c24784a6e49
Thomas.dryjard <newwbuiss@gmail.com>
2020-06-02 16:58:09 +0200

    delete cors

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const app = require('express')();
 require('dotenv').config();
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const api = require('./routes');
 
 const port = process.env.PORT || 8080
-
-app.use(cors({origin: 'https://sortouch.co'}))
 
 app.use(bodyParser.json());
 
