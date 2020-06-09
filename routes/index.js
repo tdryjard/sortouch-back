902b2b11a180ee6cc0f425584f64f1100ac7c23e
Thomas.dryjard <newwbuiss@gmail.com>
2020-06-09 23:59:10 +0200

    up index

--- routes/index.js
+++ routes/index.js
@@ -33,41 +33,70 @@ router.use('/user', user)
 
 router.use('/contact', contact)
 
-router.use('/v1/customers', (req, res) => {
-    stripe.customers.create(
-        {
-          description: 'Customer Sortouch',
-          email: req.body.email
-        },
-        function(err, customer) {
-          // asynchronously called
-          console.log(customer)
-        }
-      );
-})
+router.use('/create-customer', async (req, res) => {
+  // Create a new customer object
+  const customer = await stripe.customers.create({
+    email: req.body.email,
+  });
+
+  // Recommendation: save the customer.id in your database.
+  res.send({ customer });
+});
 
 router.use('/create-subscription', async (req, res) => {
-    try {
-      await stripe.paymentMethods.attach(req.body.paymentMethodId, {
-        customer: req.body.customerId,
-      });
-    } catch (error) {
-      return res.status('402').send({ error: { message: error.message } });
-    }
-    await stripe.customers.update(
-      req.body.customerId,
-      {
-        invoice_settings: {
-          default_payment_method: req.body.paymentMethodId,
-        },
-      }
-    );
-    const subscription = await stripe.subscriptions.create({
+  try {
+    await stripe.paymentMethods.attach(req.body.paymentMethodId, {
       customer: req.body.customerId,
-      items: [{ price: 'price_H1NlVtpo6ubk0m' }],
-      expand: ['latest_invoice.payment_intent'],
     });
-    res.send(subscription);
+  } catch (error) {
+    return res.status('402').send({ error: { message: error.message } });
+  }
+  await stripe.customers.update(
+    req.body.customerId,
+    {
+      invoice_settings: {
+        default_payment_method: req.body.paymentMethodId,
+      },
+    }
+  );
+  const subscription = await stripe.subscriptions.create({
+    customer: req.body.customerId,
+    items: [{ price: 'price_1GrQ6bKleZ50Ivn665vv20Bt' }],
+    expand: ['latest_invoice.payment_intent'],
   });
+  res.send(subscription);
+});
+
+router.use('/create-subscription-2', async (req, res) => {
+  try {
+    await stripe.paymentMethods.attach(req.body.paymentMethodId, {
+      customer: req.body.customerId,
+    });
+  } catch (error) {
+    return res.status('402').send({ error: { message: error.message } });
+  }
+  await stripe.customers.update(
+    req.body.customerId,
+    {
+      invoice_settings: {
+        default_payment_method: req.body.paymentMethodId,
+      },
+    }
+  );
+  const subscription = await stripe.subscriptions.create({
+    customer: req.body.customerId,
+    items: [{ price: 'price_1GrXyuKleZ50Ivn63Ue0o7ZA' }],
+    expand: ['latest_invoice.payment_intent'],
+  });
+  res.send(subscription);
+});
+
+router.use('/cancel-subscription', async (req, res) => {
+  // Delete the subscription
+  const deletedSubscription = await stripe.subscriptions.del(
+    req.body.subscriptionId
+  );
+  res.send(deletedSubscription);
+});
 
 module.exports = router;
